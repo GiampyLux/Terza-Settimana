ef4260fda51a94526d2aa5d12df49f310bf82444
GiampyLux <163867101+GiampyLux@users.noreply.github.com>
2024-04-10 16:58:31 +0200

    Add files via upload

---
+++ Esercizio 10-04-2024/js/script.js
@@ -0,0 +1,15 @@
+const handleSubmit = function (e) {
+    e.preventDefault()
+    aggTask()
+    taskComplete()
+    deleteTask()
+}
+
+const aggTask = function() {
+    let btn = document.getElementById("taskInput")
+    
+    
+
+
+    
+} 
